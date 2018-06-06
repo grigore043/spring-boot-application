1625910852df5624a73d49447503ecbb3aca1f8d
Grigore Rusnac <grigore043@gmail.com>
2018-06-06 19:31:33 +0300

    Stucture rafactoring

---
+++ course-api/src/main/java/com/test/spring/boot/CourseApiApp.java
@@ -0,0 +1,15 @@
+package com.test.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main( String[] args ) {
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
