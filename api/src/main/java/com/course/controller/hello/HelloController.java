abfa06b7cb3e8046cade95b15df6d2d71149d777
grusnac <en.grigore.rusnac@gmail.com>
2019-11-04 13:36:56 +0200

    Fixed dependency issue.

--- api/src/main/java/com/course/controller/hello/HelloController.java
+++ api/src/main/java/com/course/controller/hello/HelloController.java
@@ -1,9 +1,9 @@
 package com.course.controller.hello;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/")
